589fa9b1d42c8509509b0907636e346dce2b4940
Gaurav_Jha <gaurav.jha.20@mountblue.tech>
2022-05-18 22:32:33 +0530

    questin 4 solved

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,88 @@
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.util.*;
-import java.util.stream.IntStream;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         List<Map<String,String>> matches = readFile("matches.csv");
         List<Map<String,String>> deliveries = readFile("deliveries.csv");
 //        question 1
-        /*
 
         Map<String,Integer>mpy=matchesPlayedPerYear(matches);
         System.out.println(mpy);
 
-         */
 
 //        question 2
-        /*
 
         Map<String,Integer>matchesWon=MatchesWon(matches);
         System.out.println(matchesWon);
 
-         */
 
 //        question 3
 
-        Map<String,Integer>matchesWon=MatchesWon(matches);
-        System.out.println(matchesWon);
+        Map<String,Integer> extras = extra2016(matches,deliveries);
+        System.out.println(extras);
 
+//        question  4
+
+        Map<String,Integer>echoBaller=echomomy(matches,deliveries);
+        System.out.println(echoBaller);
 
     }
 
-//    calculate matches won overyears
+    private static Map<String, Integer> echomomy(List<Map<String, String>> matches, List<Map<String, String>> deliveries) {
+        ArrayList<String> matchId= getMatchesId(matches,"2015");
+        Map<String, Integer>data=new HashMap<>();
+        for (int i = 0; i < deliveries.size(); i++) {
+            if (matchId.contains(deliveries.get(i).get("match_id"))) {
+                int runs = Integer.parseInt(deliveries.get(i).get("total_runs"));
+                String current_Baller = deliveries.get(i).get("bowler");
+                if (data.containsKey(current_Baller)) {
+                    data.put(current_Baller, data.get(current_Baller) + runs);
+                } else {
+                    data.put(current_Baller, runs);
+                }
+            }
+        }
+        return data;
+    }
+
+//
+// calculate extras
+
+    private static Map<String, Integer> extra2016(List<Map<String, String>> matches, List<Map<String, String>> deliveries) {
+        ArrayList<String> matchId= getMatchesId(matches,"2016");
+        Map<String,Integer> data=new HashMap<>();
+        for (int i = 0; i < deliveries.size(); i++) {
+            if(matchId.contains(deliveries.get(i).get("match_id"))){
+                String team=deliveries.get(i).get("bowling_team");
+                int score= Integer.parseInt(deliveries.get(i).get("extra_runs"));
+                if(score>0){
+                    if(data.containsKey(team)){
+                        data.put(team,data.get(team)+score);
+                    }else{
+                        data.put(team,score);
+                    }
+                }
+            }
+        }
+        return data;
+    }
+
+//filter matches by year
+    private static ArrayList<String> getMatchesId(List<Map<String, String>> matches, String s) {
+        ArrayList<String> matchId= new ArrayList<>();
+        for (int i = 0; i < matches.size(); i++) {
+            if(matches.get(i).get("season").equals(s)){
+                matchId.add(matches.get(i).get("id"));
+            }
+        }
+        return matchId;
+    }
+
+
+    //    calculate matches won over years
     private static Map<String, Integer> MatchesWon(List<Map<String, String>> matches) {
         Map<String, Integer> data=new HashMap<>();
         for(int i=0;i<matches.size();i++){
